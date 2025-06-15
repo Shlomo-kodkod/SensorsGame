14cd6e26fd20aaac4bc66f066f4fca233c72c214
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-15 18:09:51 +0300

    add  InitGame class

---
+++ SensorsGame/InitGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class InitGame
+    {
+        Random random= new Random();
+       
+
+        internal Sensor[] InitSensors(int sensorSlots)
+        {
+            Sensor[] sensorsOptions = new Sensor[sensorSlots];
+
+            for (int i = 0; i <= sensorSlots; i++)
+            {
+                int choice = random.Next(0, 1);
+                switch (choice)
+                {
+                    case 0:
+                        sensorsOptions.Append(new AudioSensor());
+                        break;
+                    
+                }
+            }
+
+            return sensorsOptions;
+        }
+
+        internal IranianAgent InitAgent()
+        {
+            IranianAgent result = null;
+            int choice = random.Next(0, 1);
+            switch (choice)
+            {
+                case 0:
+                    result = new FootSoldier(InitSensors(2));
+                    break;
+
+            }
+
+            return result;
+        }
+
+
+    }
+}
