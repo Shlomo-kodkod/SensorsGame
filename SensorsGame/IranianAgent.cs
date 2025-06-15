a04e10122f778833e525db6731e1325f6273ac6a
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-15 12:43:17 +0300

    add IranianAgent class

---
+++ SensorsGame/IranianAgent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal abstract class IranianAgent
+    {
+        Sensor[] sensors;
+
+        string type;
+
+        internal abstract int GetSensorsCount();
+        internal abstract Sensor[] GetAgentSensors();
+        internal abstract bool IsCorrect(Sensor sensor);
+        internal abstract bool IsExposed(Sensor[] sensors);
+
+
+    }
+}
