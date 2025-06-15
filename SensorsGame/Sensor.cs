cbe46d9965dc581579a7d21d6404288ff8fe33a3
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-15 12:25:43 +0300

    add sensore class

---
+++ SensorsGame/Sensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal abstract class Sensor
+    {
+        string type;
+
+        internal abstract string Activate();
+    }
+}
