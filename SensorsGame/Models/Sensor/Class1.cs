12ec268bbf2ab1e849b7b643a48bc3ee1a006bb0
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-16 16:32:18 +0300

    add MotionSensor

---
+++ SensorsGame/Models/Sensor/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class MotionSensor : Sensor
+    {
+        public MotionSensor()
+        {
+            this.type = "Motion Sensor";
+        }
+
+        public override void Activate(IranianAgent agent, Sensor sensor)
+        {
+            if (agent.IsCorrect(this.type))
+            {
+                agent.AddSensore(sensor);
+                agent.UpdateExposedNum();
+            }
+        }
+    }
+}
