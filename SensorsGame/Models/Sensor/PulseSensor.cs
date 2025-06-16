36039a04f5d4dbe0b8900fbb7b3129303d9016f1
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-16 15:42:49 +0300

    add pulse sensore class

---
+++ SensorsGame/Models/Sensor/PulseSensor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class PulseSensor : Sensor
+    {
+        private bool IsBroken = false;
+        public PulseSensor()
+        {
+            this.type = "Pulse Sensor";
+        }
+
+        public override void Activate(IranianAgent agent, Sensor sensor)
+        {
+            if (agent.IsCorrect(this.type))
+            {
+                agent.AddSensore(sensor);
+                agent.UpdateExposedNum();
+            }
+        }
+
+        public bool GetStatus()
+        {
+            return this.IsBroken;
+        }
+        public void ChangeStatus()
+        {
+            this.IsBroken = true;
+        }
+    }
+}
