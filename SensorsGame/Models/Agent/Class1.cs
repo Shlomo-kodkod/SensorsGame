bc95a1e54e00855d4ced2a2cfb9d7a04635c6c77
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-16 18:21:20 +0300

    add OrganizationLeader

---
+++ SensorsGame/Models/Agent/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class OrganizationLeader : IranianAgent
+    {
+        public OrganizationLeader(string[] sensor) : base(sensor)
+        {
+            this.rank = "Organization Leader";
+            this.sensorSlots = 8;
+            this.IsAttack = true;
+            this.attackNum = 1;
+        }
+    }
+}
