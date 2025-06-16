c8ab8ec71caa84fcc214179d1d428bfca0ce83f6
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-16 18:19:09 +0300

    add SeniorCommander

---
+++ SensorsGame/Models/Agent/SeniorCommander.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class SeniorCommander : IranianAgent
+    {
+        public SeniorCommander(string[] sensor) : base(sensor)
+        {
+            this.rank = "Senior Commander";
+            this.sensorSlots = 6;
+            this.IsAttack = true;
+            this.attackNum = 2;
+        }
+    }
+}
