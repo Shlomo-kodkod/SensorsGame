9f17fe499b8607696c037745c28899bd7433d4e8
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-16 17:33:26 +0300

    add  LightSensor

---
+++ SensorsGame/Models/Sensor/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class LightSensor : Sensor
+    {
+        public override void Activate(IranianAgent agent, Sensor sensor)
+        {
+
+            if (agent.IsCorrect(this.type))
+            {
+                Console.WriteLine("You've uncovered information about the agent.\n" + 
+                    $"Agent rank is: {agent.rank}\n" +
+                    $"Agent Sensor slots is: {agent.sensorSlots}"
+                    );
+                agent.AddSensore(sensor);
+                agent.UpdateExposedNum();
+            }
+        }
+    }
+}
