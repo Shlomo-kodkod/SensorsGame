2d9f7497c2240af39ad69acf19bd5b4e91065471
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-17 16:30:37 +0300

    add players class

---
+++ SensorsGame/Models/Players.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal class Players
+    {
+        public string userName;
+        public string secretPass;
+        public int gameLevel;
+        public string agentType;
+        public Players(string UserName, string SecretPass, int GameLevel, string AgentType)
+        {
+            this.userName = UserName;
+            this.secretPass = SecretPass;
+            this.gameLevel = GameLevel;
+            this.agentType = AgentType;
+        }
+    }
+}
