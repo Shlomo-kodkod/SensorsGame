20ee0ea346843ea1e10938f860e150af88c70138
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-06-18 11:32:01 +0300

    add sql logs

---
+++ SensorsGame/DAL/FileLog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsGame
+{
+    internal static class FileLog
+    {
+        private static DateTime GetTime()
+        {
+            return DateTime.Now;
+        }
+
+        private static string CreatLog(string user, string log)
+        {
+            return $"[{GetTime()}] - {user} - {log}.";
+        }
+
+        public static void LogToFile(string user, string log)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("Log.txt", true, Encoding.ASCII))
+                {
+                    string fullLog = CreatLog(user, log);
+                    sw.WriteLine(fullLog);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+            
+
+        }
+
+
+    }
+}
